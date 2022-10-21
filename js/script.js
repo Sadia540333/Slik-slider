0c61279605a46b8680fde29161e79f58791f30bf
sadia554466 <pue.khulna@gmail.com>
2022-10-21 17:30:07 +0600

    Class-14

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// $(".open").click(function(){
+//     $(".one").slideToggle()
+// })
+
+$(".banner").slick()
+
+
+// =============fadeToggle-slider===================
+
+$(".open").click(function(){
+    $(".five").slideToggle()
+})
